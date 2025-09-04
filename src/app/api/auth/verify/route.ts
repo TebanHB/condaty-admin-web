b4fb118a5999f0f78ebfc5bea566f2514ce1082d
TebanHB <e_s_t_e_b_a_n-hurtado@hotmail.com>
2025-09-04 16:32:53 -0400

    Modificando la verificacion para que reciba desde al app movil o web

--- src/app/api/auth/verify/route.ts
+++ src/app/api/auth/verify/route.ts
@@ -1,22 +1,33 @@
-// src/app/api/auth/verify/route.ts
-import { NextResponse } from 'next/server';
+import { NextResponse, NextRequest } from 'next/server';
 import { cookies } from 'next/headers';
 import jwt from 'jsonwebtoken';
 
+export async function GET(request: NextRequest) {
+    let tokenValue: string | undefined;
 
-export async function GET() {
-    const cookieStore = await cookies();
-    const token = cookieStore.get('authToken');
+    const authHeader = request.headers.get('Authorization');
+    if (authHeader?.startsWith('Bearer ')) {
+        tokenValue = authHeader.split(' ')[1];
+    }
+
+    if (!tokenValue) {
+        // <<-- CAMBIO AQUÍ
+        const cookieStore = await cookies();
+        const tokenCookie = cookieStore.get('authToken');
+        tokenValue = tokenCookie?.value;
+    }
+
     const JWT_SECRET = process.env.JWT_SECRET;
     if (!JWT_SECRET) {
-        throw new Error('La variable de entorno JWT_SECRET no está definida en .env.local');
+        throw new Error('La variable de entorno JWT_SECRET no está definida');
     }
-    if (!token) {
+
+    if (!tokenValue) {
         return NextResponse.json({ message: 'No autenticado' }, { status: 401 });
     }
 
     try {
-        const decoded = jwt.verify(token.value, JWT_SECRET);
+        const decoded = jwt.verify(tokenValue, JWT_SECRET);
         return NextResponse.json(decoded, { status: 200 });
     } catch (error) {
         console.error('JWT verification error:', error);
